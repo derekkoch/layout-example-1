1778fa2b899662a95a4207830593d2165be9b667
derekkoch <derek@indie-soft.com>
2017-04-27 12:12:06 -0700

    Update files for Tab Menu design pattern

--- js/page.js
+++ js/page.js
@@ -1,6 +1,20 @@
 $(document).ready(function(e) {
-	$('.rollup-trigger').click(function(e){
-		$(this).closest('.rollup').children().not('.rollup-trigger').toggleClass('hidden');
-		$(this).parent().removeClass('hidden');
+	$('ul.tabs li').click(function(e){
+		$('ul.tabs li.selected').removeClass('selected');
+		$(this).addClass('selected');
+		
 	});
+
+	$('ul.tabs li .tab-label').mouseover(function(e) {
+		$(this).addClass('hover');
+	});
+
+	$('ul.tabs li .tab-label').mouseout(function(e) {
+		$(this).removeClass('hover');
+	});
+
+	$('ul.tabs .tab-label a').click(function(e) {
+		e.preventDefault();
+		$this.blur();
+	})
 });
